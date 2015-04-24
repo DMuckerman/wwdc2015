c3100f89327e7f231b4a578dce11170ffecf911f
Daniel Muckerman <danielmuckerman@me.com>
2015-04-24 05:49:51 -0400

    Initial Commit

---
+++ Daniel Muckerman/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Daniel Muckerman
+//
+//  Created by Daniel Muckerman on 4/24/15.
+//  Copyright (c) 2015 Daniel Muckerman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
